8491b14d90dc22ec94ac75a7bc78ab18466878a6
sckoorp <chkamalennour@gmail.com>
2024-10-28 16:27:18 +0100

    ls1: read files

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+# Reading files
+def read_file():
+    # Open file
+    file = open("users.txt", "r")
+
+    # Read file
+    # content = file.read()
+    # print(content)
+
+    # Read file lines
+    lines = file.readlines()
+
+    for line in lines:
+        print(line)
+
+    # Close file
+    file.close()
+
 def main():
+    read_file()
     return
 
 if __name__ == "__main__":
