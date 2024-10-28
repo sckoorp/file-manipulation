9076e9a2a1f4825469bbe90395940e94a545bfbb
sckoorp <chkamalennour@gmail.com>
2024-10-28 16:51:40 +0100

    ls3: write & read files

--- main.py
+++ main.py
@@ -32,9 +32,33 @@ def write_file(filename: str):
     # Close file
     file.close()
 
+# Write & Read files
+def write_read_file(filename: str):
+    # Data to write
+    groceries = ["Apples", "Bananas", "Oranges", "Peaches"]
+
+    # Open file
+    file = open(filename, "w+")
+
+    # Write file
+    for i in groceries:
+        # "\n" new line character
+        file.write(i + "\n")
+    
+    # Reset cursor (if we don't reset cursor it will just return an empty res)
+    file.seek(0, 0)
+
+    # Read file
+    content = file.read()
+    print(content)
+
+    # Close file
+    file.close()
+
 def main():
     # read_file()
-    write_file("stack.txt")
+    # write_file("stack.txt")
+    write_read_file("groceries.txt")
     return
 
 if __name__ == "__main__":
